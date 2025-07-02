d5d03461762b1ece62d279ddd34841b205e8b94f
safdar-azeem <safdarazeem00@gmail.com>
2025-07-02 16:47:08 +0500

    Add debounce.ts

---
+++ src/utils/debounce.ts
@@ -0,0 +1,13 @@
+export function debounce<T extends (...args: any[]) => void>(
+	func: T,
+	wait: number
+): (...args: Parameters<T>) => void {
+	let timeout: ReturnType<typeof setTimeout>
+
+	return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+		clearTimeout(timeout)
+		timeout = setTimeout(() => {
+			func.apply(this, args)
+		}, wait)
+	}
+}
