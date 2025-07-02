d9c4afc7a822b92477a3e5383eac77095c2d84c6
safdar-azeem <safdarazeem00@gmail.com>
2025-07-02 16:47:07 +0500

    Add PaginationFraction.vue

---
+++ src/components/Pagination/PaginationFraction.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface PaginationFractionProps {
+   currentIndex: number
+   totalSlides: number
+}
+
+defineProps<PaginationFractionProps>()
+</script>
+
+<template>
+   <div class="carousel-pagination-fraction carousel-pagination-items">
+      <span class="carousel-pagination-fraction-current">{{ currentIndex + 1 }}</span>
+      <span class="carousel-pagination-fraction-separator">/</span>
+      <span class="carousel-pagination-fraction-total">{{ totalSlides }}</span>
+   </div>
+</template>
