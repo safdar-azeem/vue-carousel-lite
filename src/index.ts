fe2c4c6f84ea288238fe2d867f6050a12b46e416
safdar-azeem <safdarazeem00@gmail.com>
2025-07-02 16:47:08 +0500

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import Carousel from './components/Carousel.vue'
+
+export { initializeDevice } from './composables/initializeDevice'
+
+export { type CarouselProps } from './types/index'
+export { type PaginationType } from './types/index'
+export { type PaginationPosition } from './types/index'
+export { type CarouselMethods } from './types/index'
+export { type SlidesPerView } from './types/index'
+
+export default Carousel
