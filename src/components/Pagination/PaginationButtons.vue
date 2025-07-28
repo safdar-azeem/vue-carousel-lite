a9bd033d0795fa9718bcba941e2f735301d7d5d2
safdar-azeem <safdarazeem00@gmail.com>
2025-07-28 23:51:48 +0500

    Update PaginationButtons.vue

--- src/components/Pagination/PaginationButtons.vue
+++ src/components/Pagination/PaginationButtons.vue
@@ -29,14 +29,21 @@ const handleButtonClick = (direction: 'prev' | 'next') => {
          :disabled="!canGoPrev"
          :aria-label="'Previous slide'"
          @click.stop="handleButtonClick('prev')">
-         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-               d="M15 18L9 12L15 6"
-               stroke="currentColor"
-               stroke-width="2"
-               stroke-linecap="round"
-               stroke-linejoin="round" />
-         </svg>
+         <slot name="prev-icon">
+            <svg
+               width="24"
+               height="24"
+               viewBox="0 0 24 24"
+               fill="none"
+               xmlns="http://www.w3.org/2000/svg">
+               <path
+                  d="M15 18L9 12L15 6"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round" />
+            </svg>
+         </slot>
       </button>
 
       <button
@@ -48,14 +55,21 @@ const handleButtonClick = (direction: 'prev' | 'next') => {
          :disabled="!canGoNext"
          :aria-label="'Next slide'"
          @click.stop="handleButtonClick('next')">
-         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-               d="M9 18L15 12L9 6"
-               stroke="currentColor"
-               stroke-width="2"
-               stroke-linecap="round"
-               stroke-linejoin="round" />
-         </svg>
+         <slot name="next-icon">
+            <svg
+               width="24"
+               height="24"
+               viewBox="0 0 24 24"
+               fill="none"
+               xmlns="http://www.w3.org/2000/svg">
+               <path
+                  d="M9 18L15 12L9 6"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round" />
+            </svg>
+         </slot>
       </button>
    </div>
 </template>
